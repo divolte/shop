f4abdff5c1cbbd9a6bfbc5bd4d13003de1650955
Barend Garvelink <bgarvelink@xebia.com>
2017-12-01 15:44:17 +0100

    Don't fail on HTTPError when getting Top Pick ⛏

--- webapp/handlers/homepage_handler.py
+++ webapp/handlers/homepage_handler.py
@@ -3,6 +3,8 @@ from tornado.httpclient import HTTPRequest,AsyncHTTPClient
 from tornado.escape import json_decode
 from datetime import timedelta
 
+from tornado.web import HTTPError
+
 from .handler_base import ShopHandler
 
 class HomepageHandler(ShopHandler):
@@ -14,7 +16,7 @@ class HomepageHandler(ShopHandler):
             response = yield with_timeout(timedelta(milliseconds=15), http.fetch(request))
             winner = json_decode(response.body)
             top_item = yield self._get_json('catalog/item/%s' % winner)
-        except (OSError, ConnectionRefusedError, TimeoutError):
+        except (OSError, ConnectionRefusedError, TimeoutError, HTTPError):
             top_item = None
 
         self.render(
