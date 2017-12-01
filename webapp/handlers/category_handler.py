6f35cf522823d3d004753e8d1eb5dba2e36b27bb
Barend Garvelink <bgarvelink@xebia.com>
2017-12-01 15:29:06 +0100

    Handle empty catalog

--- webapp/handlers/category_handler.py
+++ webapp/handlers/category_handler.py
@@ -1,20 +1,29 @@
+import logging
+
 from tornado.gen import coroutine
-from itertools import zip_longest
+from tornado.httpclient import HTTPError
 
 from .handler_base import ShopHandler
+
 
 class CategoryHandler(ShopHandler):
     @coroutine
     def get(self, name, page):
-        categories = yield self._get_json(
-            'catalog/category/%s' % name,
-            page=int(page) if page else 0,
-            size=self.config.ITEMS_PER_PAGE)
-
-        self.render(
-            'category.html',
-            items=categories['items'],
-            page=categories['page'],
-            items_per_page=self.config.ITEMS_PER_PAGE,
-            total=categories['total'],
-            name=categories['name'])
+        try:
+            categories = yield self._get_json(
+                'catalog/category/%s' % name,
+                page=int(page) if page else 0,
+                size=self.config.ITEMS_PER_PAGE)
+            self.render(
+                'category.html',
+                items=categories['items'],
+                page=categories['page'],
+                items_per_page=self.config.ITEMS_PER_PAGE,
+                total=categories['total'],
+                name=categories['name'])
+        except HTTPError as e:
+            logging.error("Catalogue not found")
+            if e.code == 404:
+                self.render('catalog-not-initialised.html')
+            else:
+                raise e
