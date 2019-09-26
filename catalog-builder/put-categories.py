9f0c10654fa780c1e3cc1a094d18c7b18c189206
Cor Zuurmond <jczuurmond@protonmail.com>
2019-09-26 13:54:56 +0200

    Solve flake8 issues

--- catalog-builder/put-categories.py
+++ catalog-builder/put-categories.py
@@ -27,9 +27,10 @@ def make_item(json, filename):
                 'flickr_url': size['url'],
                 'img_source': size['source']
                 }
-            for name,size in json['sizes'].items()
+            for name, size in json['sizes'].items()
         }
     }
+
 
 def merge_items(*input):
     items = list(filter(lambda x: x, input))
@@ -43,9 +44,11 @@ def merge_items(*input):
     result = {}
     result.update(items[0])
     for item in items[1:]:
-        result['categories'] = list(set(result['categories'] + item['categories']))
+        result['categories'] = list(
+            set(result['categories'] + item['categories']))
 
     return result
+
 
 def with_price(item, min_favs, max_favs):
     r = (max_favs - min_favs + 1) / 2.0
@@ -56,19 +59,19 @@ def with_price(item, min_favs, max_favs):
 
     return result
 
+
 def main(args):
     print('Items from:\n\t%s' % '\n\t'.join(args.files))
 
     item_list = reduce(
-        lambda x,y: x+y,
-        [[
-            make_item(json.loads(line), fn)
-        for line in open(fn).readlines()]
-        for fn in args.files])
+        lambda x, y: x+y,
+        [[make_item(json.loads(line), fn) for line in open(fn).readlines()]
+         for fn in args.files])
 
     min_favs = min([item['favs'] for item in item_list])
     max_favs = max([item['favs'] for item in item_list])
-    priced_item_list = [with_price(item, min_favs, max_favs) for item in item_list]
+    priced_item_list = [
+        with_price(item, min_favs, max_favs) for item in item_list]
 
     items = {}
     for item in priced_item_list:
@@ -76,7 +79,10 @@ def main(args):
 
     responses = Counter()
     for item in items.values():
-        r = requests.put(args.api_base_url + '/catalog/item', data=json.dumps(item), headers={'Content-Type': 'application/json'})
+        r = requests.put(
+            args.api_base_url + '/catalog/item',
+            data=json.dumps(item),
+            headers={'Content-Type': 'application/json'})
         print('PUT %s, %d' % (item['id'], r.status_code))
         if r.status_code != 200:
             print(json.dumps(item))
@@ -84,14 +90,28 @@ def main(args):
 
     print('\nSummary:')
     for status, count in responses.items():
-       print('\t%d times status code %d' % (count,status))
+        print('\t%d times status code %d' % (count, status))
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Take downloaded category JSON files and push them into the Catalog API.')
-    parser.add_argument('--api-base-url', '-u', metavar='API_BASE_URL', type=str, default='http://localhost:8080/api', help='The base URL of the Catalog API.')
+    parser = argparse.ArgumentParser(
+        description=(
+            'Take downloaded category JSON files and push them into the '
+            'Catalog API.'
+        )
+    )
+    parser.add_argument(
+        '--api-base-url',
+        '-u',
+        metavar='API_BASE_URL',
+        type=str,
+        default='http://localhost:8080/api',
+        help='The base URL of the Catalog API.'
+    )
     parser.add_argument('files', metavar='JSON_FILE', type=str, nargs='+')
 
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse_args()
