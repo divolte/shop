1791e1d5d696b7129942e73a27c4d38a1e574e07
Alessandro Gentile <alemaudit@gmail.com>
2020-01-28 14:14:51 +0100

    placeholder for the method to insert the checkout record

--- webapp/handlers/download_handler.py
+++ webapp/handlers/download_handler.py
@@ -2,10 +2,13 @@ from tornado.gen import coroutine
 
 from .handler_base import ShopHandler
 
+
 class DownloadHandler(ShopHandler):
     @coroutine
     def get(self, id):
         checkout = yield self._get_json('checkout/completed/%s' % id)
+        # checkout is ready to be consumed and stored in the database
         self.render(
             'download.html',
-            items=checkout['basket']['items'])
+            items=checkout['basket']['items']
+        )
